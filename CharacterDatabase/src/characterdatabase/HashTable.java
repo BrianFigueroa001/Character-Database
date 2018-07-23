f55e73ffe71e07f7dfe8c53123b50c5b8264fd84
BrianFigueroa001 <brianfigueroa001@gmail.com>
2018-07-23 15:48:55 -0400

    Reverting

--- CharacterDatabase/src/characterdatabase/HashTable.java
+++ CharacterDatabase/src/characterdatabase/HashTable.java
@@ -6,37 +6,45 @@ public class HashTable {
     private int numOfCharacters; //tracks number of characters in hashtable
 
     public void put(String key, Character charObj){
+        if (isTableFull()){
+            System.out.println("\nERROR: DATABASE FULL");
+            return;
+        }
         HashEntry newEntry = new HashEntry(key, charObj);
         int index = hashFunction(key);
-        while (TABLE[index] != null){ //linear probing
-            if (TABLE[index].getKey().equals(key)){
-                System.out.println("\nERROR: ENTRY INSERTION FAILED");
-                return;
-            }
-            else if (index == MAX - 1){ //Problem: Unable to insert a character in a available position before the index. Realistically should never happen
-                System.out.println("\nERROR: ENTRY INSERTION FAILED");
-                return;
-            }
-            else {
-                ++index;
-            }
+        if (TABLE[index] == null){
+            TABLE[index] = newEntry;
+            numOfCharacters += 1;
+            return;
         }
-        TABLE[index] = newEntry;
-        numOfCharacters += 1;
+        if (isEqual(index, key)){
+            System.out.println("\nERROR: ENTER NEW KEY");
+            return;
+        }
+        int initialIndex = index;
+        index = putLinearProbing(index, key);
+        if (index == initialIndex){
+            return;
+        }
+        else {
+            TABLE[index] = newEntry;
+            numOfCharacters += 1;
+        }
     }
     
     public Character get(String key){
         int index = hashFunction(key);
         if (TABLE[index] == null){
+            System.out.println("\nERROR: ENTRY NOT FOUND");
             return null;
+
         }
-        while (!TABLE[index].getKey().equals(key)) { //linear probing
-            ++index;
-            if (index == MAX) {
-                System.out.println("\nERROR: ENTRY NOT FOUND");
-                return null;
-            }
+        if (isEqual(index, key)){
+            return TABLE[index].getCharacter();
         }
+        int tracker = index;
+        index = linearProbing(index, key);
+
         return TABLE[index].getCharacter();
     }
 
@@ -46,13 +54,13 @@ public class HashTable {
             System.out.println("\nERROR: ENTRY NOT FOUND");
             return;
         }
-        while (!TABLE[index].getKey().equals(key)){
-            ++index;
-            if (index == MAX){
-                System.out.println("\nERROR: ENTRY NOT FOUND");
-                return;
-            }
+        if (isEqual(index, key)){
+            TABLE[index] = null;
+            return;
         }
+        int tracker = index;
+        index = linearProbing(index, key);
+
         TABLE[index] = null;
         System.out.println("\nEntry Deleted From Database");
         numOfCharacters -= 1;
@@ -71,5 +79,40 @@ public class HashTable {
         return Math.abs(key % MAX);
     }
 
-    
+    private int linearProbing(int index, String key){ //for delete() and get()
+        int tracker = index;
+        ++index;
+        while (TABLE[index] == null){
+
+        }
+    }
+
+    private int putLinearProbing(int index, String key){ //for put() method. It searches for a null element in characterTable
+        int initialIndex = index;
+        ++index;
+        while (TABLE[index] != null){ //linear probing
+            if (index == initialIndex) {
+                System.out.println("\nERROR: DATABASE IS FULL");
+                return initialIndex;
+            }
+            if (isEqual(index, key)){
+                System.out.println("\nERROR: ENTER NEW KEY");
+                return initialIndex;
+            }
+            else if (index == MAX - 1){ //Problem: Unable to insert a character in a available position before the index. Realistically should never happen
+                index = 0;
+            }
+            else {
+                ++index;
+            }
+        }
+        return index;
+    }
+    private boolean isEqual(int index, String key){
+        return TABLE[index].getKey().equals(key);
+    }
+
+    private boolean isTableFull(){
+        return numOfCharacters == MAX;
+    }
 }
